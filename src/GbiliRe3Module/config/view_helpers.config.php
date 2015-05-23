a86cbb860ba5471112ca937dbe5d150a33389b4f
Guillermo Devi <g@gbili.com>
2015-05-23 16:14:14 +0200

    scriptalicious as invokable

--- src/GbiliRe3Module/config/view_helpers.config.php
+++ src/GbiliRe3Module/config/view_helpers.config.php
@@ -3,7 +3,7 @@ namespace GbiliRe3Module;
 return array(
     'factories' => array(
         'resolveScript' => function ($vhm) {
-            $scriptalicious = $vhm->getServiceLocator()->get('scriptalicious');
+            $scriptalicious = $vhm->getServiceLocator()->get('Scriptalicious');
             $vh = new View\Helper\Re3($scriptalicious);
             return $vh;
         },

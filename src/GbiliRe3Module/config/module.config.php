e602d4561bc7d2c613eaaea6adfbd6c7abc30fe2
Guillermo Devi <g@gbili.com>
2015-05-23 15:45:35 +0200

    scriptalicious as invokable

--- src/GbiliRe3Module/config/module.config.php
+++ src/GbiliRe3Module/config/module.config.php
@@ -3,4 +3,5 @@ namespace GbiliRe3Module;
 
 return array(
     'view_helpers'       => include __DIR__ . '/view_helpers.config.php',
+    'service_manager'       => include __DIR__ . '/service_manager.config.php',
 );
---
+++ src/GbiliRe3Module/config/service_manager.config.php
@@ -0,0 +1,6 @@
+<?php
+return [
+    'invokables' => [
+        'scriptalicious' => '\Gbili\Stdlib\Scriptalicious', 
+    ],
+];
